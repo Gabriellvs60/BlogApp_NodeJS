049476a5bbfa93770781e225642d2902611c8b43
Gabriel Coelho <gabriellvs60@gmail.com>
2019-08-06 10:19:58 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+//Carregando Módulos
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const admin = require("./routes/admin")
+//const mongoose = require('mongoose')
+
+//Configurações
+    //Body Parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+    //Handlebars
+    app.engine('handlebars',handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars');
+//Mongoose
+//
+
+//Rotas
+    app.use('/admin', admin)
+//Outros
+const PORT = 8089
+app.listen(PORT, () =>{
+    console.log("Servidor Rodando! ")
+})
