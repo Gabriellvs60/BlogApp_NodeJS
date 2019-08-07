be12853d832495be450827608caec378a9fc226a
Gabriel Coelho <gabriellvs60@gmail.com>
2019-08-07 20:07:11 -0300

    Session no middleware
    Criando variável global no middleware para trabalhar com mensagens de sucesso.

--- App.js
+++ App.js
@@ -6,8 +6,23 @@ const app = express()
 const admin = require("./routes/admin")
 const path = require("path")
 const mongoose = require('mongoose')
+const session = require("express-session")
+const flash = require("connect-flash")
 
 //Configurações
+//Sessão
+    app.use(session({
+        secret: "cursodenode",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+    //Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
     //Body Parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
@@ -24,6 +39,12 @@ const mongoose = require('mongoose')
 //Public
 app.use(express.static(path.join(__dirname, "public")))
 //Rotas
+
+//MIDDLEWARE
+app.use((req, res, next) =>{
+    console.log("Oi, eu sou um Midleware")
+    next()
+})
     app.use('/admin', admin)
 //Outros
 const PORT = 8089
