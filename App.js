118f51c63fe7be691d2815a4c1362d8a47806910
Gabriel Coelho <gabriellvs60@gmail.com>
2019-08-13 19:20:03 -0300

    Configurando passport
    Configuração do passport

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ const Postagem = mongoose.model("postagens")
 require("./models/Categoria")
 const Categoria = mongoose.model("categorias")
 const usuarios = require("./routes/usuario")
+const passport = require("passport")
+require("./config/auth")(passport)
+
 //Configurações
 //Sessão
     app.use(session({
@@ -20,6 +23,9 @@ const usuarios = require("./routes/usuario")
         resave: true,
         saveUninitialized: true
     }))
+
+    app.use(passport.initialize())
+    app.use(passport.session())
     app.use(flash())
     //Middleware
     app.use((req, res, next) => {
